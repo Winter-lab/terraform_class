9ef9a142431fc0908bbbf13648a56451ecdf8a68
Petit_Bourge0is <75848627+Winter-lab@users.noreply.github.com>
2024-02-20 13:39:52 +0100

    Update backend.tf

--- tp1/tp1-main/03-encrypted-team-work/backend.tf
+++ tp1/tp1-main/03-encrypted-team-work/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "fr-ippon-formation-terraform-tps-common-remote-backend-bucket"
     key            = "tp1/team-work" # Check this
-    region         = "eu-west-1"
+    region         = "eu-west-3"
     dynamodb_table = "fr-ippon-formation-terraform-tps-common-remote-backend-dynamo"
   }
 }
