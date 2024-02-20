ac42a45ba71b7ddbea0511274fb0f0a9a859efe8
Olivier Moreau <m242@tutanota.com>
2024-02-20 17:20:35 +0100

    networking bis

--- part3/main.tf
+++ part3/main.tf
@@ -3,6 +3,9 @@
 # Create a VPC
 resource "aws_vpc" "vpc_paris" {
   cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "vpc_groupe8"
+  }
 }
 
 
@@ -66,5 +69,47 @@ resource "aws_subnet" "sn5" {
 
   tags = {
     Name = "subnet_5"
+  }
+}
+
+// EC2 pool
+resource "aws_launch_template" "lt" {
+  name_prefix   = "groupe8"
+  image_id      = "ami-1a2b3c"
+  instance_type = "t2.micro"
+}
+
+resource "aws_autoscaling_group" "ec2_pool" {
+  availability_zones = ["eu-west-3a", "eu-west-3b", "eu-west-3c"]
+  desired_capacity   = 1
+  max_size           = 1
+  min_size           = 1
+
+  launch_template {
+    id      = aws_launch_template.lt.id
+    version = "$Latest"
+  }
+}
+
+// Internet Gateway
+resource "aws_internet_gateway" "ig" {
+  vpc_id = "vpc_paris"
+  tags = {
+    Name = "vpc_paris_igw"
+  }
+}
+
+// Elastic IP for NAT
+resource "aws_eip" "nat_eip" {
+  vpc        = true
+  depends_on = [aws_internet_gateway.ig]
+}
+// NAT
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = [aws_subnet.sn0.id, aws_subnet.sn1.id, aws_subnet.sn2.id]
+  depends_on    = [aws_internet_gateway.ig]
+  tags = {
+    Name = "nat"
   }
 }
